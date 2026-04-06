4c08419f8da2ef1e9e339358051754735c99b2dc
LE BAR <letruong8017@gmail.com>
2026-04-06 16:09:41 +0700

    feat: create prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    const linkUrl = configService.get<string>('DATABASE_URL');
+    const pool = new Pool({ connectionString: linkUrl });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected DB success!');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
